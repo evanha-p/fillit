63236cf180bb35261c00901458d38a7339ab618e
Eero Vanha-perttula <evanha-p@c2r6p11.hive.fi>
2022-03-02 17:24:57 +0200

    cleaned the functions a little bit. Still no norm

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 15:10:31 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/03/02 15:37:43 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/03/02 16:32:30 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_var
+{
+	int		x;
+	int		y;
+	int		i;
+	int		letter;
+}	t_var;
+
 typedef struct s_block	t_block;
 struct	s_block
 {
@@ -29,7 +37,6 @@ struct	s_block
 };
 
 int		get_next_line(const int fd, char **line);
-t_block	*char2blocks(char **arr);
 t_block	*char2list(char **arr);
 char	**read_file(char *argv);
 void	print_shape(t_block shape);
